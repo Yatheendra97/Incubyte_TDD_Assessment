7817dab468c58e710e7aa9edd887fec6d8169a44
Yatheendra Panchumarthi <yatheendra.cse@gmail.com>
2025-07-23 16:16:18 +0530

    RED: Test for custom delimiter support

--- lib/test/string_calculator_test.dart
+++ lib/test/string_calculator_test.dart
@@ -33,6 +33,11 @@ void main() {
       expect(calculator.add('1\n2\n3'), equals(6));
     });
 
+    test('should support custom delimiters', () {
+      expect(calculator.add('//;\n1;2'), equals(3));
+      expect(calculator.add('//|\n1|2|3'), equals(6));
+    });
+
 
   });
 }
