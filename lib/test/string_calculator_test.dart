e015792d408ebc813c21684a24983c22e8df8776
Yatheendra Panchumarthi <yatheendra.cse@gmail.com>
2025-07-23 15:58:59 +0530

    RED: Test for single number input

--- lib/test/string_calculator_test.dart
+++ lib/test/string_calculator_test.dart
@@ -12,5 +12,12 @@ void main() {
     test('should return 0 for empty string', () {
       expect(calculator.add(''), equals(0));
     });
+
+    test('should return the number itself for single number', () {
+      expect(calculator.add('1'), equals(1));
+      expect(calculator.add('5'), equals(5));
+    });
+
+
   });
 }
